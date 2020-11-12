2419460b69081436bfe148e385eb23a400290e57
s v <chofexx@gmail.com>
2020-11-12 05:44:30 +0200

    Adding Pie Model

---
+++ src/BethanysPieShop/BethanysPieShop/Models/Pie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShop.Models
+{
+    public class Pie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageURI { get; set; }
+
+        public double Price { get; set; }
+
+        public bool InStock { get; set; }
+    }
+}
